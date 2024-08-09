bf1a59e584ba5e537351e837773d0ab3c3b9793f
wp <wp245219@outlook.com>
2024-08-09 10:59:14 +0800

    结构改为自定义插件包

---
+++ Packages/com.punversal.editorexplugin/Runtime/EditorFramework/Runtime/TypeEx.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using UnityEngine;
+
+namespace EditorFramework
+{
+    public static class TypeEx
+    {
+        public static IEnumerable<Type> GetSubTypesInAssemblies(this Type self)
+        {
+            return AppDomain.CurrentDomain.GetAssemblies()
+            .Where(assembly=> assembly.FullName.StartsWith("Punversal"))
+            .SelectMany(assembly => assembly.GetTypes())
+            .Where(type => type.IsSubclassOf(self));
+        }
+
+        public static IEnumerable<Type> GetSubTypesWithClassAttributeInAssemblies<TClassAttribute>(this Type self) where TClassAttribute : Attribute
+        {
+            return GetSubTypesInAssemblies(self)
+            .Where(type => type.GetCustomAttribute<TClassAttribute>() != null);
+        }
+    }
+}
+
