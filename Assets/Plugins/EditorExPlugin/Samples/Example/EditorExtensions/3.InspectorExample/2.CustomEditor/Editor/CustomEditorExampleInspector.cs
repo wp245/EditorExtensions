3e751a9a8e862d82c5de5710496127997845a294
wp <wp245219@outlook.com>
2024-08-12 16:20:43 +0800

    inspector editor载体

---
+++ Assets/Plugins/EditorExPlugin/Samples/Example/EditorExtensions/3.InspectorExample/2.CustomEditor/Editor/CustomEditorExampleInspector.cs
@@ -0,0 +1,29 @@
+using UnityEditor;
+using UnityEngine;
+
+namespace EditorExtensions
+{
+    [CustomEditor(typeof(CustomEditorExample), false)]
+    public class CustomEditorExampleInspector: Editor
+    {
+        CustomEditorExample mTarget
+        {
+            get {return target as CustomEditorExample;}
+        }
+        public override void OnInspectorGUI()
+        {
+            var hpRect = GUILayoutUtility.GetRect(18, 18, "TextField");
+            EditorGUI.ProgressBar(hpRect, mTarget.HP, "HP");
+            var rect = GUILayoutUtility.GetRect(18,18,"TextField");
+            EditorGUI.ProgressBar(rect, mTarget.Range, "Range");
+
+            EditorGUILayout.BeginHorizontal("box");
+            EditorGUILayout.LabelField("角色名字", GUILayout.Width(100));
+            mTarget.RoleName = EditorGUILayout.TextArea(mTarget.RoleName);
+            EditorGUILayout.EndHorizontal();
+            
+            EditorGUILayout.ObjectField(serializedObject.FindProperty("OtherObj"));
+            serializedObject.ApplyModifiedProperties();
+        }
+    }
+}
