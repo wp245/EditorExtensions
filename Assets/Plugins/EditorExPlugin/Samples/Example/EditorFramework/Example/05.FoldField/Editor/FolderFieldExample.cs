86381643cefcdcd7a7f1372d8da36be5e8955a57
wp <wp245219@outlook.com>
2024-08-15 17:48:09 +0800

    文件槽绘制

--- Assets/Plugins/EditorExPlugin/Samples/Example/EditorFramework/Example/05.FoldField/Editor/FolderFieldExample.cs
+++ Assets/Plugins/EditorExPlugin/Samples/Example/EditorFramework/Example/05.FoldField/Editor/FolderFieldExample.cs
@@ -1,4 +1,5 @@
 using System;
+using System.IO;
 using UnityEditor;
 using UnityEngine;
 
@@ -7,22 +8,25 @@ namespace EditorFramework
     [CustomEditorWindow(5)]
     public class FolderFieldExample: EditorWindow
     {
-        private string mPath = "";
+        private string mPath = "Assets";
         private void OnGUI()
         {
             var rect = EditorGUILayout.GetControlRect(GUILayout.Height(20));
             var rects = rect.VerticalSplit(rect.width - 30);
             var leftRect  = rects[0];
             var rightRect = rects[1];
-            
-            
-            GUI.Label(leftRect,mPath, "box");
-            
+
+            var currentGUIEnable = GUI.enabled;
+            GUI.enabled = false;
+            EditorGUI.TextField(leftRect,mPath);
+            GUI.enabled = currentGUIEnable;
             
             if (GUI.Button(rightRect, GUIContents.Folder))
             {
                 var path = EditorUtility.OpenFolderPanel("打开文件", Application.dataPath, "default name");
-                mPath = path;
+
+                var assetFullPath = Path.GetFullPath(Application.dataPath);
+                mPath = "Assets" + Path.GetFullPath(path).Substring(assetFullPath.Length).Replace("\\", "/");
                 Debug.Log(path);
             }
 
