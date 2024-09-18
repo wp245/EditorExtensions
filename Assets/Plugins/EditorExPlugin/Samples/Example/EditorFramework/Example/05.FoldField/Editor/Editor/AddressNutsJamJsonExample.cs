b85f38d4512cca3a912b70a184825261ace017a3
wp <wp245219@outlook.com>
2024-09-18 15:43:40 +0800

    处理拧螺丝游戏json关卡配置的编辑器扩展工具

--- Assets/Plugins/EditorExPlugin/Samples/Example/EditorFramework/Example/05.FoldField/Editor/Editor/AddressNutsJamJsonExample.cs
+++ Assets/Plugins/EditorExPlugin/Samples/Example/EditorFramework/Example/05.FoldField/Editor/Editor/AddressNutsJamJsonExample.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.IO;
+using Newtonsoft.Json;
 using UnityEditor;
 using UnityEngine;
 using NutsJamEditorEx;
@@ -7,7 +8,7 @@ using NutsJamEditorEx;
 namespace EditorFramework
 {
     [CustomEditorWindow(6)]
-    public class AddressNutsJamJsonExample: EditorWindow
+    public class AddressNutsJamJsonExample : EditorWindow
     {
         private string mReadJsonPath = "";
         private GameLevelConfig mGameLevelConfig = new GameLevelConfig();
@@ -31,6 +32,7 @@ namespace EditorFramework
                     {
                         mReadJsonPath = EditorUtility.OpenFilePanel("选择文件", "", "json");
                     }
+
                     // 打开文件所在目录
                     if (GUILayout.Button("打开文件所在目录"))
                     {
@@ -38,64 +40,82 @@ namespace EditorFramework
                     }
                 }
                 GUILayout.EndHorizontal();
-                
+
                 GUILayout.Space(100);
 
                 GUILayout.BeginHorizontal();
                 {
-                    GUILayout.Label("生成关卡数量");
-                    mGenLevelCount = GUILayout.TextField(mGenLevelCount);
-                    GUILayout.Label("每关层数");
-                    mLayersPerLevel = GUILayout.TextField(mLayersPerLevel);
-                    GUILayout.Label("关卡基数");
-                    mNewLevelPre = GUILayout.TextField(mNewLevelPre);
-                    if (GUILayout.Button("读取json文件并生成关卡json"))
+                    GUILayout.BeginVertical();
                     {
-                        string text = File.ReadAllText(mReadJsonPath);
-                        mGameLevelConfig = JsonUtility.FromJson<GameLevelConfig>(text);
-                        var levelDSingleBoardCount = mGameLevelConfig.data.Count;
-                        var configData = mGameLevelConfig.data;
-                        var genLevelCount = int.Parse(mGenLevelCount);
-                        var layersPerLevel = int.Parse(mLayersPerLevel);
-                        var newLevelPre = int.Parse(mNewLevelPre);
-                        GameLevelConfig newGameLevelConfig = new GameLevelConfig();
-                        for (int i = 1; i <= genLevelCount; i++)
-                        {
-                            var tempGameChildLevelData = new GameChildLevelData()
-                            {
-                                totalLayer = 0,
-                                freeHoleCount = 5,
-                                haveTimeLimit = false,
-                                limitTime = 0,
-                                allUnderNailData = new List<GameNailData>(),
-                                allGameBoard = new List<GameBoardData>(),
-                                gameNailBoxData = new List<GameNailBoxData>(),
-                            };
-                            for (int j = 1; j < layersPerLevel; j++)
-                            {
-                                int randomIndex = Random.Range(1, levelDSingleBoardCount+1);
-                                
-                                foreach (var gameChildLevelData in configData)
-                                {
-                                   if(int.Parse(gameChildLevelData.Key)%10000 == randomIndex)
-                                   {
-                                       gameChildLevelData.Value.allGameBoard[0].belongLayer = j;
-                                       tempGameChildLevelData.allGameBoard.Add(gameChildLevelData.Value.allGameBoard[0]);
-                                   }
-                                }
-                            }
-                            newGameLevelConfig.data.Add((newLevelPre+i).ToString(), tempGameChildLevelData);
-                        }
-                        // 获取文件所在文件夹的路径
-                        var folderPath = Path.GetDirectoryName(mReadJsonPath);
-                        // 获取文件名
-                        var fileName = "new_new_new_" + Path.GetFileName(mReadJsonPath);
-                        // 生成新的json文件
-                        File.WriteAllText(Path.Combine(folderPath, "new_" + fileName), JsonUtility.ToJson(newGameLevelConfig));
-                        
+                        GUILayout.Label("生成关卡数量");
+                        mGenLevelCount = GUILayout.TextField(mGenLevelCount);
                     }
+                    GUILayout.EndVertical();
+                    GUILayout.BeginVertical();
+                    {
+                        GUILayout.Label("每关层数");
+                        mLayersPerLevel = GUILayout.TextField(mLayersPerLevel);
+                    }
+                    GUILayout.EndVertical();
+                    GUILayout.BeginVertical();
+                    {
+                        GUILayout.Label("关卡基数");
+                        mNewLevelPre = GUILayout.TextField(mNewLevelPre);
+                    }
+                    GUILayout.EndVertical();
                 }
                 GUILayout.EndHorizontal();
+                if (GUILayout.Button("读取json文件并生成关卡json"))
+                {
+                    string text = File.ReadAllText(mReadJsonPath);
+                    mGameLevelConfig = JsonConvert.DeserializeObject<GameLevelConfig>(text);
+                    var levelDSingleBoardCount = mGameLevelConfig.data.Count;
+                    var configData = mGameLevelConfig.data;
+                    var genLevelCount = int.Parse(mGenLevelCount);
+                    var layersPerLevel = int.Parse(mLayersPerLevel);
+                    var newLevelPre = int.Parse(mNewLevelPre);
+                    GameLevelConfig newGameLevelConfig = new GameLevelConfig();
+                    for (int i = 1; i <= genLevelCount; i++)
+                    {
+                        var tempGameChildLevelData = new GameChildLevelData()
+                        {
+                            totalLayer = 0,
+                            freeHoleCount = 5,
+                            haveTimeLimit = false,
+                            limitTime = 0,
+                            allUnderNailData = new List<GameNailData>(),
+                            allGameBoard = new List<GameBoardData>(),
+                            gameNailBoxData = new List<GameNailBoxData>(),
+                        };
+                        for (int j = 1; j <= layersPerLevel; j++)
+                        {
+                            int randomIndex = Random.Range(1, levelDSingleBoardCount + 1);
+
+                            foreach (var gameChildLevelData in configData)
+                            {
+                                if (int.Parse(gameChildLevelData.Key) % 10000 == randomIndex)
+                                {
+                                    foreach (var gameBoardData in gameChildLevelData.Value.allGameBoard)
+                                    {
+                                        var data = JsonConvert.DeserializeObject<GameBoardData>(JsonConvert.SerializeObject(gameBoardData));
+                                        data.belongLayer = j;
+                                        tempGameChildLevelData.allGameBoard.Add(data);
+                                    }
+                                }
+                            }
+                        }
+
+                        newGameLevelConfig.data.Add((newLevelPre + i).ToString(), tempGameChildLevelData);
+                    }
+
+                    // 获取文件所在文件夹的路径
+                    var folderPath = Path.GetDirectoryName(mReadJsonPath);
+                    // 获取文件名
+                    var fileName = "new_new_new_" + Path.GetFileName(mReadJsonPath);
+                    // 生成新的json文件
+                    File.WriteAllText(Path.Combine(folderPath, "new_" + fileName),
+                        JsonConvert.SerializeObject(newGameLevelConfig));
+                }
             }
             GUILayout.EndVertical();
         }
